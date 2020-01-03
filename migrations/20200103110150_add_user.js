e5ef0c3702ea961e88819c75f882302979187b3c
Capi15 <capicapi1515@gmail.com>
2020-01-03 11:13:58 +0000

    added user migration

---
+++ migrations/20200103110150_add_user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+exports.up = async knex => {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+  await knex.schema
+    .createTable('Users', table => {
+      table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('password').notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    throw new Error('Irreversible Migration');
+};
